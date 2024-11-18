a84c1b2a8144e999795fc0945e8bab02709a0328
Daws <castrogiraldosantiago@gmail.com>
2024-11-18 12:38:22 -0500

    Feat: projectDAo methods Creation

--- src/main/java/com/volunteer/persistance/DAO/ProjectDAO.java
+++ src/main/java/com/volunteer/persistance/DAO/ProjectDAO.java
@@ -1,9 +1,24 @@
 package com.volunteer.persistance.DAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.volunteer.entities.Project;
+
 public class ProjectDAO {
     private String table= "projects";
     //TODO Create a project
-    
+    public void addProject(){
+        
+
+    }
     //TODOlist all projects avaliables
+    public List<Project> getAllProjects(){
+        List<Project> projects= new ArrayList<Project>();
+        //TODO definition
+        
+        return  projects;
+
+    }
 
 }
