644c3154a82cb09518658e015bb5e34536884e6e
Daws <castrogiraldosantiago@gmail.com>
2024-11-18 13:33:37 -0500

    Feat: ProjectDao Creation Implenetation

--- src/main/java/com/volunteer/persistance/DAO/ProjectDAO.java
+++ src/main/java/com/volunteer/persistance/DAO/ProjectDAO.java
@@ -1,14 +1,35 @@
 package com.volunteer.persistance.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.volunteer.entities.Project;
+import com.volunteer.persistance.db.DbContext;
 
 public class ProjectDAO {
     private String table= "projects";
-    //TODO Create a project
-    public void addProject(){
+    
+    //Create a project
+    public void addProject(Project objProject){
+        String query=String.format("INTSERT INTO %s (title,description,start_date,end_date,created_by) VALUES (?,?,?,?,?)",table);
+        try (
+            Connection con= DbContext.getConeection();
+            PreparedStatement statement = con.prepareStatement(query);
+            )
+            {
+                statement.setString(1, objProject.getTitle());
+                statement.setString(2, objProject.getDescription());
+                statement.setDate(3,java.sql.Date.valueOf(objProject.getStartDate()));
+                statement.setDate(4,java.sql.Date.valueOf(objProject.getEndDate()));
+                statement.setString(5, objProject.getTitle());
+                statement.executeLargeUpdate();
+
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         
 
     }
