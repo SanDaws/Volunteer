372a183cac8cf4c2b4bec1971b13da25b6d9240b
Daws <castrogiraldosantiago@gmail.com>
2024-11-18 10:19:51 -0500

    db context

---
+++ src/main/java/com/volunteer/persistance/db/DbContext.java
@@ -0,0 +1,20 @@
+package com.volunteer.persistance.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbContext {
+    private static final String URL="jdbc:mysql://bq7lfmhsgycb8t3mvy0t-mysql.services.clever-cloud.com:3306/bq7lfmhsgycb8t3mvy0t";
+    private static final String USER="uwf8za0y2qsgfmlp";
+    private static final String PASSWORD="efdgkLH3jfYkePdm5F8w";
+    
+    private DbContext(){}
+    
+    public static Connection  getConeection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    
+    }
+
+}
+
