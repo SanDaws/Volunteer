dda293ba53f273dc5c7dc847520c810eaeda375e
Daws <castrogiraldosantiago@gmail.com>
2024-11-18 12:30:32 -0500

    Feat: inscription and prjects DAOs creation

---
+++ src/main/java/com/volunteer/persistance/DAO/InscriptionDAO.java
@@ -0,0 +1,9 @@
+package com.volunteer.persistance.DAO;
+
+public class InscriptionDAO {
+    private String table= "inscriptoins";
+    //TODOonly volunteers inscribe to a project
+    //TODOvolunteers inscriptios record
+    //TODOrecord of volunnteers inscripted to a project 
+
+}
--- src/main/java/com/volunteer/persistance/DAO/ProjectDAO.java
+++ src/main/java/com/volunteer/persistance/DAO/ProjectDAO.java
@@ -1,5 +1,9 @@
 package com.volunteer.persistance.DAO;
 
 public class ProjectDAO {
+    private String table= "projects";
+    //TODO Create a project
+    
+    //TODOlist all projects avaliables
 
 }
