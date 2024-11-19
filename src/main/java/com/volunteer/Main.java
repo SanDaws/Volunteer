c4805513a7aa127bb0ce844202df63fb90991fff
Daws <castrogiraldosantiago@gmail.com>
2024-11-18 23:05:46 -0500

    Menu implementation

--- src/main/java/com/volunteer/Main.java
+++ src/main/java/com/volunteer/Main.java
@@ -1,7 +1,12 @@
 package com.volunteer;
 
+import com.volunteer.entities.User;
+import com.volunteer.views.Auth.Menu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("works");
+       Menu mn=new Menu();
+       User user= mn.Menu();
+       
     }
 }
