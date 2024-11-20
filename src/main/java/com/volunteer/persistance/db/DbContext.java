4d9fd165604a419bd9ae042d0e1b20946766ba44
Daws <castrogiraldosantiago@gmail.com>
2024-11-20 10:17:15 -0500

    FEAT:  context description

--- src/main/java/com/volunteer/persistance/db/DbContext.java
+++ src/main/java/com/volunteer/persistance/db/DbContext.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 
 public class DbContext {
+    //jdbc:mysql://HOST:PORT/DATABASE_NAME
     private static final String URL="jdbc:mysql://bq7lfmhsgycb8t3mvy0t-mysql.services.clever-cloud.com:3306/bq7lfmhsgycb8t3mvy0t";
     private static final String USER="uwf8za0y2qsgfmlp";
     private static final String PASSWORD="efdgkLH3jfYkePdm5F8w";
